f8363b3917af71bd7dff745164f1f958d4fd708f
hharris56 <hvharris@calpoly.edu>
2020-02-08 22:38:33 -0500

    merge from master

--- app.js
+++ app.js
@@ -19,17 +19,35 @@ log.warning("Log level warning");
 async function quickstart() {
     // Imports the Google Cloud client library
     const vision = require('@google-cloud/vision');
- 
+
     // Creates a client
     const client = new vision.ImageAnnotatorClient();
- 
+
     // Performs label detection on the image file
-    const [result] = await client.labelDetection('./can.jpeg');
-    const labels = result.labelAnnotations;
+    img = './imgs/egg carton 3.jpg';
+
+    const [logo_result] = await client.logoDetection(img);
+    const logos = logo_result.logoAnnotations;
+    console.log('Logos:');
+    logos.forEach(logo => console.log(logo.description));
+
+    console.log()
+
+    const [label_result] = await client.labelDetection(img);
+    const labels = label_result.labelAnnotations;
     console.log('Labels:');
     labels.forEach(label => console.log(label.description));
+
+    const [ob_result] = await client.objectLocalization(img);
+    const objects = ob_result.localizedObjectAnnotations;
+
+    objects.forEach(object =>
+        console.log(object.))
+    // console.log('First of Labels: ');
+    // console.log(getFirst(labels).description)
 }
 
+<<<<<<< Updated upstream
 // POST /lable?location=lat,long
 // BODY: jpg image compressed
 app.post('/lable', function (req, res) {
@@ -44,3 +62,16 @@ app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 // quickstart();
 
+=======
+// accepts list, returns highest match, throws error if list empty
+function getFirst(a) {
+    try {
+        return a[0];
+    } catch (err) {
+        console.log('No labels detected');
+    }
+}
+
+
+quickstart()
+>>>>>>> Stashed changes
